8de3e067443e24fdd8c7945edbfbe381bbc2fb08
Wesley-waka <wesley.waka@student.moringaschool.com>
2023-02-11 16:41:19 +0300

    Done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
 
+class Book
+    attr_reader  :title
+    attr_accessor  :author,:page_count,:genre
+
+
+    def initialize(title)
+        @title=title
+    end
+
+    def change_info(n,h,w)
+            self.author = n
+            self.page_count = h
+            self.genre = w
+        end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
